a2bebf7816dd83f7e45daf7479e18de5d0bb72c4
Valentine <v.zhyvotchenko@gmail.com>
2022-06-29 13:41:03 +0300

    added the ability to add and remove list items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,68 @@
+import {Component} from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
+
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John S.', salary: 800, increase: true, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: false, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3},
+      ]
+    }
 
-  const data = [
-    {name: 'John S.', salary: 800, increase: true, id: 1},
-    {name: 'Alex M.', salary: 3000, increase: false, id: 2},
-    {name: 'Carl W.', salary: 5000, increase: false, id: 3},
-  ];
+    this.maxId = 4;
+  }
 
-  return (
-    <div className="app">
-      <AppInfo/>
+  deleteItem = (id) => {
+    this.setState(({data}) => {        
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
 
-      <div className="search-panel">
-        <SearchPanel/>
-        <AppFilter/>
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++
+    }
+
+    this.setState(({data}) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo/>
+
+        <div className="search-panel">
+          <SearchPanel/>
+          <AppFilter/>
+        </div>
+
+        <EmployersList 
+            data={this.state.data}
+            onDelete={this.deleteItem}/>
+        <EmployersAddForm onAdd={this.addItem}/>
       </div>
-
-      <EmployersList data={data}/>
-      <EmployersAddForm/>
-    </div>
-  ); 
+    ); 
+  }
 }
 
 export default App;
